df4cb596f819cbf3adcadc5c5f4243e0568661b3
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2023-11-08 11:45:46 +0100

    add logged in user data to profile page and logout

--- frontend/lib/pages/Profile.dart
+++ frontend/lib/pages/Profile.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/Home.dart';
 import 'package:frontend/widgets/update_profile_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../widgets/profile_menu_widget.dart';
 
@@ -13,14 +15,47 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  late SharedPreferences preferences;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserData();
+  }
+
+  void getUserData() async {
+    setState(() {
+      isLoading = true;
+    });
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void logout() {
+    preferences.clear();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => const Home(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const Home(),
+                  ),
+                );
               },
               icon: const Icon(Icons.arrow_left_outlined)),
           title: const Text(
@@ -37,101 +72,109 @@ class _ProfileState extends State<Profile> {
             ),
           ],
           elevation: 0,
-          backgroundColor: Colors.red[900],
+          // backgroundColor: Colors.red[900],
           titleSpacing: 2,
         ),
         body: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    const SizedBox(
-                      width: 120,
-                      height: 120,
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage('assets/profile.jpg'),
-                        radius: 60,
+          child: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Text(preferences.getString('token').toString()),
+                      Stack(
+                        children: [
+                          const SizedBox(
+                            width: 120,
+                            height: 120,
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage('assets/profile.jpg'),
+                              radius: 60,
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Container(
+                              width: 35,
+                              height: 35,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.grey,
+                              ),
+                              child: const Icon(
+                                Icons.edit,
+                                color: Colors.black,
+                                size: 20,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: Colors.grey,
-                        ),
-                        child: const Icon(
-                          Icons.edit,
-                          color: Colors.black,
-                          size: 20,
+                      const SizedBox(height: 10),
+                      Text(preferences.getString('name').toString()),
+                      Text(preferences.getString('email').toString()),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const UpdateProfileScreen()),
+                          ),
+                          child: const Text("Edit profile"),
                         ),
                       ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 10),
-                const Text("Dummy Name"),
-                const Text('Dummy Email'),
-                const SizedBox(height: 20),
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UpdateProfileScreen()),
-                    ),
-                    child: const Text("Edit profile"),
+                      const SizedBox(height: 30),
+                      const Divider(),
+                      const SizedBox(height: 10),
+                      ProfileMenuWidget(
+                        title: 'Settings',
+                        icon: Icons.settings,
+                        textColor: Colors.black,
+                        onPress: () {},
+                      ),
+                      ProfileMenuWidget(
+                        title: 'Billing Details',
+                        icon: Icons.wallet_outlined,
+                        textColor: Colors.black,
+                        onPress: () {},
+                      ),
+                      ProfileMenuWidget(
+                        title: 'User Management',
+                        icon: Icons.verified_user,
+                        textColor: Colors.black,
+                        onPress: () {},
+                      ),
+                      const Divider(
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ProfileMenuWidget(
+                        title: 'Information',
+                        icon: Icons.info,
+                        textColor: Colors.black,
+                        onPress: () {},
+                      ),
+                      ProfileMenuWidget(
+                        title: 'Logout',
+                        icon: Icons.logout_outlined,
+                        onPress: () {
+                          logout();
+                        },
+                        textColor: Colors.red,
+                        endIcon: false,
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 30),
-                const Divider(),
-                const SizedBox(height: 10),
-                ProfileMenuWidget(
-                  title: 'Settings',
-                  icon: Icons.settings,
-                  textColor: Colors.black,
-                  onPress: () {},
-                ),
-                ProfileMenuWidget(
-                  title: 'Billing Details',
-                  icon: Icons.wallet_outlined,
-                  textColor: Colors.black,
-                  onPress: () {},
-                ),
-                ProfileMenuWidget(
-                  title: 'User Management',
-                  icon: Icons.verified_user,
-                  textColor: Colors.black,
-                  onPress: () {},
-                ),
-                const Divider(
-                  color: Colors.grey,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ProfileMenuWidget(
-                  title: 'Information',
-                  icon: Icons.info,
-                  textColor: Colors.black,
-                  onPress: () {},
-                ),
-                ProfileMenuWidget(
-                  title: 'Logout',
-                  icon: Icons.logout_outlined,
-                  onPress: () {},
-                  textColor: Colors.red,
-                  endIcon: false,
-                ),
-              ],
-            ),
-          ),
         ));
   }
 }

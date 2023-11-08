5f4a5d3aa46f0130f792cd77de9722426fe76e0e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2023-11-08 11:43:58 +0100

    add registration page

---
+++ frontend/lib/pages/Register.dart
@@ -0,0 +1,143 @@
+// ignore_for_file: file_names, use_build_context_synchronously
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:frontend/methods/api.dart';
+import 'package:frontend/pages/Login.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  TextEditingController email = TextEditingController();
+  TextEditingController name = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  void registerUser() async {
+    final data = {
+      'name': name.text.toString(),
+      'email': email.text.toString(),
+      'password': password.text.toString(),
+    };
+    final result = await API().postRequest(route: '/register', data: data);
+    final response = jsonDecode(result.body);
+    if (response['status'] == 200) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const Login(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.grey[400],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child: Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 300,
+                  child: TextFormField(
+                    controller: name,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    cursorColor: Colors.white,
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      labelStyle: TextStyle(color: Colors.white),
+                      iconColor: Colors.white,
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelText: 'Enter your username',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 300,
+                  child: TextFormField(
+                    controller: email,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    cursorColor: Colors.white,
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      labelStyle: TextStyle(color: Colors.white),
+                      iconColor: Colors.white,
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelText: 'Enter your email',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  width: 300,
+                  child: TextFormField(
+                    controller: password,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    cursorColor: Colors.white,
+                    decoration: const InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white)),
+                      labelStyle: TextStyle(color: Colors.white),
+                      iconColor: Colors.white,
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelText: 'Enter your password',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[600],
+                      ),
+                      onPressed: () => {
+                            registerUser(),
+                          },
+                      child: const Text("Sign up")),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

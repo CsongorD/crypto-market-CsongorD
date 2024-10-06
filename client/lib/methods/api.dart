39f585bcb913916a7846d262cd0bcd46f86a5007
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-10-06 09:40:57 +0200

    update api

--- client/lib/methods/api.dart
+++ client/lib/methods/api.dart
@@ -1,28 +1,47 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class API {
   postRequest(
       {required String route, required Map<String, String> data}) async {
-    String url = 'http://localhost:8000/api$route';
+    String host = "";
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      host = '10.0.2.2';
+    } else {
+      host = "localhost";
+    }
+
+    String url = 'http://$host:8000/api$route';
 
     var parsedUrl = Uri.parse(url);
     var response =
         await http.post(parsedUrl, body: jsonEncode(data), headers: _header());
     return response;
   }
-
   putRequest(
       {required String route, required Map<String, Object?> data}) async {
-    String url = 'http://localhost:8000/api$route';
+    String host = "";
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      host = '10.0.2.2';
+    } else {
+      host = "localhost";
+    }
+
+    String url = 'http://$host:8000/api$route';
     var parsedUrl = Uri.parse(url);
     var response =
         await http.put(parsedUrl, body: jsonEncode(data), headers: _header());
     return response;
   }
-
   getUser({required int userId}) async {
-    String url = 'http://localhost:8000/api/users/$userId';
+    String host = "";
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      host = '10.0.2.2';
+    } else {
+      host = "localhost";
+    }
+    String url = 'http://$host:8000/api/users/$userId';
     var parsedUrl = Uri.parse(url);
     var response = await http.get(parsedUrl);
     return response;

8c419d13fe57aca8b5902777b388e28ea1cf45dc
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-07-28 21:11:42 +0200

    Apply .gitignore cleanup

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Running flutter clean
 flutter clean
-
-# Fetch Flutter dependencies
 flutter pub get
 flutter pub upgrade
-
-# Enable Flutter web support
 flutter config --enable-web
 
-# Running the Flutter web app on port 8080
+# This is fine for development only
 flutter run -d web-server --web-port 8080 --web-hostname 0.0.0.0
